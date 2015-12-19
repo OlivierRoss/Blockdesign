c4fdfd23f68fa64ccfa8392c82f38650ca8c9890
Olivier Ross <olivier.ross@usherbrooke.ca>
2015-12-19 17:19:06 -0500

    modification dynamique de la taille de la matrice

--- tuiles.js
+++ tuiles.js
@@ -1,14 +1,22 @@
 /*
  * TODO ::
- * arranger probleme d'affichage des carreaux pas tous affiches
+ * conserver couleurs
  * implementer efface (clic droit) et nb couleurs limitees
  * utiliser un taille minimum pour les carres, donc pouvoir scroller dans le svg si on sort du cadre
  * implementer un zoom
  * implementer un scroll
- * mettre une image en arriere plan
  * refaire le design
+ *
+ * Etape 2
+ * mettre une image en arriere plan
  * exporter en element html par polyfill
  * calculer par panneau de cloture
+ *
+ * Etape 3
+ * Gerer l'ecriture de texte
+ *
+ * Etape 4
+ * rasterisation d'image pour cloture
  */
 
 // Variable globale
@@ -31,29 +39,27 @@ window.onload = function initialize () {
     var canvasStyle = contexte.canvas.node().style;
     contexte.svg = contexte.canvas.append("svg").attr("height", canvasStyle.height).attr("width", canvasStyle.width);
 
+    // Construction matrice + affichage
+    calculerMetriques();
+    for(var i = 0; i < contexte.nblignes; ++i)
+    {
+        var nbcarres = i % 2 === 0 ? contexte.nbcarresligne : contexte.nbcarresligne - 1;
+        contexte.matrice.push(creerligne(nbcarres));
+    }
+    
     // Evenements
     contexte.hauteur.onchange = dessiner;
     contexte.largeur.onchange = dessiner;
-
-    // Statut souris
     contexte.canvas.node().onmousedown = function () { contexte.sourisenfoncee = true };
     contexte.canvas.node().onmouseup = function () { contexte.sourisenfoncee = false };
-
-    // Affichage
-    calculerMetriques();
-    construirematrice();
-    contexte.lignes = deepcopy(contexte.matrice);
 }
 
 // Dessin
 function dessiner () {
     calculerMetriques();
+    creationmatriceaffichage();
     nettoyer();
     afficher();
-}
-
-function nettoyer () {
-    contexte.svg.selectAll("g.ligne").remove();
 }
 
 function calculerMetriques () {
@@ -61,16 +67,21 @@ function calculerMetriques () {
     contexte.nblignes = contexte.hauteur.value * contexte.carresparpied;
     contexte.nbcarresligne = contexte.largeur.value * contexte.carresparpied;
 
-    // Mise a jour de la matrice utilisee
-    contexte.lignes = deepcopy(contexte.lignes);
-    ajusterlignes();
-    //ajustercolonnes();
-
     // Calcul de cote
     var canvas = contexte.canvas.node();
-    contexte.diagonale = Math.min(canvas.offsetWidth / contexte.nbcarresligne, canvas.offsetHeight / (contexte.nblignes / 2) + (contexte.nblignes / 2));
+    contexte.diagonale = Math.min((canvas.offsetWidth - 2) / contexte.nbcarresligne, (canvas.offsetHeight - 2) / ((contexte.nblignes / 2) + 0.5));
     contexte.cote = diagonaleacote(contexte.diagonale);
     contexte.decalageRotation = (contexte.diagonale - contexte.cote) / 2;
+}
+
+function creationmatriceaffichage () {
+    contexte.lignes = deepcopy(contexte.matrice);
+    ajusterlignes();
+    ajustercolonnes();
+}
+
+function nettoyer () {
+    contexte.svg.selectAll("g.ligne").remove();
 }
 
 function ajusterlignes () {
@@ -83,28 +94,62 @@ function ajusterlignes () {
     }
     else { // il en manque
         var nb = voulu - actuel;
+        var nbcarres = contexte.matrice[0].length;
+        for(var i = 0; i < nb; ++i)
+        {
+            if(i % 2 === 0){
+                contexte.lignes.push(creerligne(nbcarres))
+                contexte.matrice.push(creerligne(nbcarres))
+            }
+            else{
+                contexte.lignes.push(creerligne(nbcarres - 1))
+                contexte.matrice.push(creerligne(nbcarres - 1))
+            }
+        }
     }
 }
 
-function ajouterligne () {
-    
+function ajustercolonnes () {
+    var voulu = contexte.nbcarresligne;
+    var actuel = contexte.matrice[0].length;
+    if(voulu === actuel) return;
+    else if (voulu < actuel) { // il y en a trop
+        var nb = actuel - voulu;
+        contexte.lignes.forEach(function (element) {
+            element.splice(-nb, nb); 
+        });
+    }
+    else { // il ny en a pas assez
+        var nb = voulu - actuel;
+        contexte.lignes.forEach(function (element) {
+            for(var i = 0; i < nb; ++i){
+                element.push({fill: "#000000", opacity: 0}); 
+            }
+        });
+        contexte.matrice.forEach(function (element) {
+            for(var i = 0; i < nb; ++i){
+                element.push({fill: "#000000", opacity: 0}); 
+            }
+        });
+    }
+}
+
+
+function creerligne (nb) {
+    var ligne = [];
+    for(var i = 0; i < nb; ++i) {
+        ligne.push({fill: "#000000", opacity: 0});
+    }
+    return ligne;
 }
  
 function construirematrice () {
-    for(var i = 0; i < contexte.nblignes; ++i)
-    {
-        var ligne = [];
-        var nbcarres = i % 2 === 0 ? contexte.nbcarresligne : contexte.nbcarresligne - 1;
-        for(var j = 0; j < nbcarres; ++j){
-            ligne.push({fill: "#000000", opacity: 0});
-        }
-        contexte.matrice.push(ligne);
-   }
 }
 
 function afficher () {
     // Creation des lignes
     var groupes = contexte.svg.selectAll("g.ligne").data(contexte.lignes).enter().append("g")
+    .attr("index", function (d, i) { return i })
     .attr("class", "ligne")
     .attr("index", function (d, i) { return i})
     .attr("transform", function (d, i) { 
@@ -118,6 +163,7 @@ function afficher () {
 
     // Creation des carres
     groupes.selectAll("rect").data(function(d) { return d; }).enter().append("rect")
+    .attr("index", function (d, i) { return i })
     .attr("class", "carre")
     .attr("index", function (d, i) { return i})
     .attr("height", contexte.cote)
@@ -141,15 +187,16 @@ function mouseover () {
 
 function changecolor () {
     var element = d3.select(this);
-    var datum = element.datum();
+    var elementNode = element.node();
+    var datum = contexte.matrice[elementNode.getAttribute("index")][elementNode.parentNode.getAttribute("index")];
     var couleur = d3.select("#color").node().value;
 
     if(datum.opacity === 0){
         datum.opacity = 1;
         element.attr("fill-opacity", datum.opacity);
 
-        element.attr("fill", couleur);
         datum.fill = couleur;
+        element.attr("fill", couleur);
         contexte.compte[couleur] = incremente(contexte.compte[couleur]);
     }
     else {
